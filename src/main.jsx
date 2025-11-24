37ccb5e99ebcbb39a36c0217e405b292edc1da2b
drekko7442 <67240119+drekko7442@users.noreply.github.com>
2025-11-23 22:59:03 -0500

    Initialize main.jsx with ReactDOM rendering

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+import PaperDungeonsDemo from "./PaperDungeonsDemo.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PaperDungeonsDemo />
+  </React.StrictMode>
+);
